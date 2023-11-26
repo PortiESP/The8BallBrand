a862cba1463e2654dc8133114d2cc7cccaee1a6b
CuB1z <d.sanrin77@gmail.com>
2023-11-26 11:39:59 +0100

    Refactor router.js and add bidCard component

--- router.js
+++ router.js
@@ -8,12 +8,23 @@ const router = express.Router()
 
 // Declare possible routes (not enabled until enabled by `app.get()`)
 router.get('/', (req, res) => res.render('index'))
-router.get('/detailed/:id', (req, res) => res.render('detailed', data[req.params.id]))
+router.get('/detailed/:id', renderDetailed)
 router.get('/publish', (req, res) => res.render('publish'))
 router.get('/legal', (req, res) => res.render('legal'))
 
 // POST routes
-router.post('/add-element', (req, res) => {
+router.post('/add-element', handleAddElement)
+router.post('/add-bid/:id', handleAddBid)
+
+//==================================================== Functions ====================================================
+function renderDetailed(req, res)  {
+  const bids = Object.values(data[req.params.id].bidders).sort((a, b) => b.bid - a.bid)   // Extract bids from data and sort them
+  const elementData = data[req.params.id]   // Extract element data from data
+
+  res.render('detailed', {...elementData, 'bids':[1,2,3]})
+}
+
+function handleAddElement (req, res) {
   const id = Date.now()
   const bidders = {
     "default": {
@@ -24,21 +35,21 @@ router.post('/add-element', (req, res) => {
     }
   }
 
-  data[id] = {id, ...req.body, bidders}
+  data[id] = { id, ...req.body, bidders }
   console.log(data)  // Debug
   res.redirect(`/detailed/${id}`)
-})
+}
 
-router.post('/add-bid/:id', (req, res) => {
+function handleAddBid (req, res) {
   const id = req.params.id
   const username = req.body.username
   const date = getDate()
 
-  data[id].bidders[username] = {...req.body, date}
+  data[id].bidders[username] = { ...req.body, date }
   console.log("BODY", req.body)  // Debug
   console.log("DATA", data)  // Debug
   res.redirect(`/detailed/${id}`)
-})
+}
 
 // Export routes definitions
 export default router
