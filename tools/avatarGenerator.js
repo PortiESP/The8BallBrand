c63666852bb4f29788254a55b0da8acba21ee8a5
danisntoss <danisant1504.dsl@gmail.com>
2023-11-27 18:25:17 +0100

    modified gravatar generation

--- tools/avatarGenerator.js
+++ tools/avatarGenerator.js
@@ -2,8 +2,7 @@ let originRoute = "https://www.gravatar.com/avatar/"
 let params = "?s=256&d=identicon"
 
 function avatarGenerator(email) {
-    let name = email.split('@')[0]
-    return originRoute + name + params
+    return originRoute + email + params
 }
 
 export default avatarGenerator
