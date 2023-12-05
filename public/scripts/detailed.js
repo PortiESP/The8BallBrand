56d9b0c43b6e42b3ae87f965fe623ec281c7a232
porti <portiesp@gmail.com>
2023-12-06 00:54:47 +0100

    Prev commit file

--- public/scripts/detailed.js
+++ public/scripts/detailed.js
@@ -1,5 +1,14 @@
+
 function toggleFav(id){
   fetch("/toggle-fav?id=" + id, {
     method: "POST"
   })
+  .then(res => res.json())
+  .then(data => {
+    document.querySelector("span.favs--bubble").innerText = data.numFavs
+  })
 }
+
+
+
+document.querySelector("label[for='favorite-checkbox']").addEventListener("click", ()=>toggleFav(new URL(location).pathname.split("/").slice(-1)[0]))
