063fa898c253e24bd04d53f5df25934c1bc4f5d8
danisntoss <danisant1504.dsl@gmail.com>
2023-12-19 18:06:18 +0100

    implemented display switch

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -1,9 +1,14 @@
+// Getting dom elements
 const $layoutButton = document.querySelector(".items--layout")
 const $layoutButtonIcon = document.querySelector(".items--layout i")
+const $itemsWrap = document.querySelector(".items--wrap")
 
+
+// Adding listeners
 $layoutButton.addEventListener("click", toggleLayout)
 
-function toggleLayout() {
+// =========================================================================================================================[ Functions ]>>>
+function toggleLayout() { 
     // TODO
     // 1.- Change layout icon
     // 2.- Change items-wrap layout
@@ -11,7 +16,13 @@ function toggleLayout() {
 
     // <i class="bi bi-view-list"></i> --> List Icon
     // <i class="bi bi-grid-fill"></i> --> Grid Icon
-
+    
+    // Toggle layout icon
     $layoutButtonIcon.classList.toggle("bi-view-list")
     $layoutButtonIcon.classList.toggle("bi-grid")
+
+    // Toggle items-wrap layout
+    $itemsWrap.classList.toggle("items-wrap--grid")
+    $itemsWrap.classList.toggle("items-wrap--list")
 }
+
