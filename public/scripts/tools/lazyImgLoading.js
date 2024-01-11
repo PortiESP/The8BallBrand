7ac30a2d992051a83a94ce3e6f9c46dbcf2059d5
porti <portiesp@gmail.com>
2024-01-11 01:12:42 +0100

    REmoved logs

--- public/scripts/tools/lazyImgLoading.js
+++ public/scripts/tools/lazyImgLoading.js
@@ -5,18 +5,15 @@ export default function lazyImgLoading(section) {
   const $products = document.querySelectorAll(section + " .item")
   const $images = document.querySelectorAll(section + " .item .item--image-front")
 
-  console.log($products)
 
   // For each image
   $images.forEach((e, i) => {
       // ------- While loading -------
       if (e.getAttribute("data-loaded")) return
-      console.log("Item loading", e, $products[i])
       e.style.opacity = 0.5
 
       // ------- When loaded -------
       e.onload = () => {
-          console.log("Item loaded", e, $products[i])
           e.style.opacity = 1
           $products[i].querySelector(".item--loading").style.display = "none"
 
